30ab554fc126b58ffd40ebf737ab28697447be29
Romman Sabbir <rommansabbir@gmail.com>
2022-04-15 00:56:20 +0600

    MainActivity :: - Tested implementation of LoggerX

--- app/src/main/java/com/rommansabbir/loggerxdemo/DemoApplication.kt
+++ app/src/main/java/com/rommansabbir/loggerxdemo/DemoApplication.kt
@@ -7,6 +7,6 @@ import com.rommansabbir.loggerx.LoggerXProvider
 class DemoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        LoggerXProvider.register(LoggerXConfig(this, true))
+        LoggerXProvider.register(LoggerXConfig(this, false))
     }
 }
--- app/src/main/java/com/rommansabbir/loggerxdemo/MainActivity.kt
+++ app/src/main/java/com/rommansabbir/loggerxdemo/MainActivity.kt
@@ -3,21 +3,22 @@ package com.rommansabbir.loggerxdemo
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.rommansabbir.loggerx.LoggerXCallback
-import com.rommansabbir.loggerx.model.DeviceInfo
-import com.rommansabbir.registerForLoggerX
+import com.rommansabbir.loggerx.registerForLoggerX
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        registerForLoggerX(
+        /*registerForLoggerX(
             object : LoggerXCallback {
                 override fun onEvent(deviceInfo: DeviceInfo, thread: Thread, throwable: Throwable) {
-                    Toast.makeText(this@MainActivity, deviceInfo.model, Toast.LENGTH_SHORT).show()
+
                 }
             }
-        )
+        )*/
+        registerForLoggerX { deviceInfo, thread, throwable ->
+            Toast.makeText(this@MainActivity, throwable.message, Toast.LENGTH_SHORT).show()
+        }
         Thread.sleep(3000)
         throw RuntimeException("Test")
     }
