d2612d5a5ab2dde91922f7b30a28043a53356c87
Romman Sabbir <rommansabbir@gmail.com>
2022-04-14 23:22:01 +0600

    Test Implementation of LoggerX :: - Tested

--- app/src/main/java/com/rommansabbir/loggerxdemo/DemoApplication.kt
+++ app/src/main/java/com/rommansabbir/loggerxdemo/DemoApplication.kt
@@ -1,12 +1,12 @@
 package com.rommansabbir.loggerxdemo
 
 import android.app.Application
-import com.rommansabbir.loggerx.LoggerXImpl
+import com.rommansabbir.loggerx.LoggerXConfig
+import com.rommansabbir.loggerx.LoggerXProvider
 
 class DemoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        val logger = LoggerXImpl()
-        logger.register(this)
+        LoggerXProvider.register(LoggerXConfig(this, true))
     }
 }
--- app/src/main/java/com/rommansabbir/loggerxdemo/MainActivity.kt
+++ app/src/main/java/com/rommansabbir/loggerxdemo/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.rommansabbir.loggerxdemo
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.rommansabbir.loggerx.LoggerXCallback
+import com.rommansabbir.loggerx.model.DeviceInfo
+import com.rommansabbir.registerForLoggerX
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        registerForLoggerX(
+            object : LoggerXCallback {
+                override fun onEvent(deviceInfo: DeviceInfo, thread: Thread, throwable: Throwable) {
+                    Toast.makeText(this@MainActivity, deviceInfo.model, Toast.LENGTH_SHORT).show()
+                }
+            }
+        )
         Thread.sleep(3000)
         throw RuntimeException("Test")
     }
