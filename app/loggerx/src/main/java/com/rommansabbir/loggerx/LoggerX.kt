f515529921cb6d6b03da1b2c68df2da93540fccd
Romman Sabbir <rommansabbir@gmail.com>
2022-04-14 23:16:57 +0600

    LoggerX :: - Refactored

--- app/loggerx/src/main/java/com/rommansabbir/loggerx/LoggerX.kt
+++ app/loggerx/src/main/java/com/rommansabbir/loggerx/LoggerX.kt
@@ -1,42 +1,11 @@
 package com.rommansabbir.loggerx
 
-import android.app.Activity
-import android.app.Application
-import android.os.Bundle
-
 interface LoggerX {
-    fun register(application: Application): Boolean
+    /*Initialize LoggerX*/
+    fun init(config: LoggerXConfig): Boolean
+
+    /*Register to get notified regarding UncaughtException events*/
+    fun registerListener(listener: LoggerXCallback?)
 }
 
-class LoggerXImpl : LoggerX {
-    // Callback for activity lifecycle for this specific application
-    private val activityCallback = object : Application.ActivityLifecycleCallbacks {
-        override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-            println("created")
-            activity.apply {
-                Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
-                    println(throwable.message)
-                }
-            }
-        }
 
-        override fun onActivityStarted(activity: Activity) {}
-
-        override fun onActivityResumed(activity: Activity) {
-        }
-
-        override fun onActivityPaused(activity: Activity) {}
-
-        override fun onActivityStopped(activity: Activity) {}
-
-        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
-
-        override fun onActivityDestroyed(activity: Activity) {}
-
-    }
-
-    override fun register(application: Application): Boolean {
-        application.registerActivityLifecycleCallbacks(activityCallback)
-        return true
-    }
-}
